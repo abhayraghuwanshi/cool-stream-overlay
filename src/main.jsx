9ac36536dac85979cbc4df00998c9a40d30180e2
Abhay raghuwanshi <45534131+abhayraghuwanshi@users.noreply.github.com>
2026-06-09 02:09:25 +0530

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+)
 
